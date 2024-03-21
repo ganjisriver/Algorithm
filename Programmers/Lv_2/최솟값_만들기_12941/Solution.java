986504045775aba292e4b70079a7519bc10f0a19
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-03-21 16:02:46 +0900

    [프로그래머스 Lv.2] 최솟값 만들기 12941

---
+++ Programmers/Lv_2/최솟값_만들기_12941/Solution.java
@@ -0,0 +1,16 @@
+package Programmers.Lv_2.최솟값_만들기_12941;
+
+import java.util.*;
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int size = A.length;
+        for (int i = 0; i < size; i++) {
+            answer += (A[i]*B[size-(i+1)]);
+        }
+
+        return answer;
+    }
+}
