4427936e4e15e8d13c60a2d4f064bbcc7294283a
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-10-24 19:06:45 +0900

    Main 클래스 추가

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Test {
+public class Main {
     public static void main(String[] args) throws IOException {
         int answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
